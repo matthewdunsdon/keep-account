a4dbf5b909f04e5e7b983e737daa84ab70429000
Matt Dunsdon <dev.mattdunsdon@gmail.com>
2023-10-21 17:02:52 +0100

    Add comments and enforcement of some checks
    The comments added to the public stuff are of poor quality.
    These comments should be improved over time.

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,17 @@
+//! keep-account is a tool to help you keep your account data organised.
 
+#![cfg_attr(doc, warn(rustdoc::all))]
+#![cfg_attr(doc, allow(rustdoc::missing_doc_code_examples))]
+#![deny(missing_docs)]
+#![warn(unreachable_pub)]
+#![allow(clippy::disallowed_names)]
+
+/// The application context
 #[derive(Default, Debug)]
 pub struct Application {}
 
 impl Application {
+    /// Performs the "general" run behaviour that is desired
     pub fn run(self) {
         println!("Hello, keep account is here to help!");
     }
